7b58eb7b04f5a4a00268b2d02b0f580303d78d2e
Diefex <diefex57@gmail.com>
2020-03-22 14:20:43 -0500

    Frontend Juego
    Se construyó el diseño básico de la pagina de un juego.

--- plataforma/js/build.js
+++ plataforma/js/build.js
@@ -1,12 +1,10 @@
-//$(document).ready(init("Biblioteca"));
-
 function init(section) {
 
     // construir Navbar
     document.getElementById("body").innerHTML = `
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: black; max-height: 100px;">
-        <a class="navbar-brand" href="#"> <img src="../imgs/MPD2G.png" alt="" height="100rem"> </a>
+        <a class="navbar-brand" href="../index.html"> <img src="../imgs/MPD2G.png" alt="" height="100rem"> </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -14,13 +12,13 @@ function init(section) {
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item `+ (section == "Tienda" ? "active" : "") + `">
-                    <a class="nav-link" href="`+ (section == "Tienda" ? "#" : "tienda.html") + `">Tienda <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="`+ (section == "Tienda" ? "#" : "tienda.html") + `">Tienda` + (section == "Tienda" ? '<span class="sr-only">(current)</span>' : "") + `</a>
                 </li>
                 <li class="nav-item `+ (section == "Biblioteca" ? "active" : "") + `">
-                    <a class="nav-link" href="`+ (section == "Biblioteca" ? "#" : "biblioteca.html") + `">Biblioteca</a>
+                    <a class="nav-link" href="`+ (section == "Biblioteca" ? "#" : "biblioteca.html") + `">Biblioteca` + (section == "Biblioteca" ? '<span class="sr-only">(current)</span>' : "") + `</a>
                 </li>
                 <li class="nav-item `+ (section == "Perfil" ? "active" : "") + `">
-                    <a class="nav-link" href="`+ (section == "Perfil" ? "#" : "perfil.html") + `">Perfil</a>
+                    <a class="nav-link" href="`+ (section == "Perfil" ? "#" : "perfil.html") + `">Perfil` + (section == "Perfil" ? '<span class="sr-only">(current)</span>' : "") + `</a>
                 </li>
             </ul>
             <button class="btn btn-outline-light mx-1">Ingresar</button>
@@ -29,7 +27,7 @@ function init(section) {
     </nav>`;
 
     // construir contenido
-    document.getElementById("body").innerHTML += `<div class="container-fluid my-2 p-md-5" id="contenido"></div>`;
+
     switch (section) {
         case "Tienda":
             initStore();
@@ -41,6 +39,10 @@ function init(section) {
 
         case "Perfil":
             initProfile();
+            break;
+
+        case "Juego":
+            initGame();
             break;
 
         default:
@@ -58,6 +60,7 @@ function init(section) {
 }
 
 function initStore() {
+    document.getElementById("body").innerHTML += `<div class="container-fluid my-2 p-md-5" id="contenido"></div>`;
     document.getElementById("contenido").innerHTML = `
     <!-- Contenido -->
     <div class="container-fluid my-2 p-md-5" id="contenido">
@@ -97,17 +100,12 @@ function initStore() {
     suggest = "";
     indicators = "";
     for (let i = 0; i < 5; i++) {
-        if (i == 0) {
-            indicators += `<li data-target="#carouselExampleIndicators" data-slide-to="` + i + `" class="active"></li>`
-            suggest += `<div class="carousel-item active">`;
-        } else {
-            indicators += `<li data-target="#carouselExampleIndicators" data-slide-to="` + i + `"></li>`
-            suggest += `<div class="carousel-item">`
-        }
+        indicators += `<li data-target="#carouselExampleIndicators" data-slide-to="` + i + `" ` + (i == 0 ? 'class="active"' : "") + `></li>`
         suggest += `
+        <div class="carousel-item `+ (i == 0 ? "active" : "") + `">
             <div class="row vh-25">
                 <div class="col">
-                    <img src="https://picsum.photos/1000/500" class="d-block w-100" alt="...">
+                    <img src="https://picsum.photos/100`+i+`/500" class="d-block w-100" alt="...">
                 </div>
                 <div class="col-md-4 text-light bg-dark container">
                     <div class="container p-3">
@@ -135,7 +133,7 @@ function initStore() {
         catalog += `
         <div class="col-md-4">
             <div class="card mb-4 bg-dark">
-                <img src="https://picsum.photos/500/255" alt="">
+                <img src="https://picsum.photos/50`+i+`/255" alt="">
                 <div class="card-body">
                     <div class="title">Game Name</div>
                     <p class="card-text">This is a wider card with supporting text below as a natural
@@ -152,6 +150,7 @@ function initStore() {
 }
 
 function initLibrary() {
+    document.getElementById("body").innerHTML += `<div class="container-fluid my-2 p-md-5" id="contenido"></div>`;
     document.getElementById("contenido").innerHTML = `
     <!-- Inicio Rapido -->
         <div class="container mb-5">
@@ -196,7 +195,7 @@ function initLibrary() {
         fast += `
         <div class="col">
             <div class="card bg-dark text-white">
-                <img src="https://picsum.photos/1000/300" class="card-img" alt="...">
+                <img src="https://picsum.photos/100`+i+`/300" class="card-img" alt="...">
                 <div class="card-img-overlay">
                     <h3 class="card-title">Game Name</h3>
                     <p class="card-text text-muted">última vez: 01 ene 2020</p>
@@ -215,7 +214,7 @@ function initLibrary() {
             <div class="card bg-dark w-100 mb-3">
                 <div class="row">
                     <div class="col-2">
-                        <img src="https://picsum.photos/100" alt="" class="d-block h-100">
+                        <img src="https://picsum.photos/10`+i+`" alt="" class="d-block h-100">
                     </div>
                     <div class="col">
                         <div class="card-body p-5">
@@ -238,4 +237,107 @@ function initLibrary() {
     }
     document.getElementById("library").innerHTML = library;
 
+}
+
+function initGame() {
+    document.getElementById("body").innerHTML += `<div class="container-fluid p-0" id="contenido"></div>`;
+    document.getElementById("contenido").innerHTML = `
+    <!-- Capturas -->
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" id="screenshots"></div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <!-- Informacion -->
+    <div class="container my-5">
+        <h1 class="display-3">Game Name</h1>
+        <div class="text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam reprehenderit perferendis quasi ratione
+            quam, odio neque voluptate itaque, vitae libero assumenda quae quisquam mollitia quaerat perspiciatis
+            pariatur sed doloremque eius!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, dolores possimus praesentium
+            expedita itaque quo blanditiis voluptate quidem labore totam deserunt omnis eligendi in quaerat
+            voluptatem nobis animi natus quis?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus provident aperiam, eaque maiores
+            vitae deleniti officia reprehenderit a amet, illo sequi possimus fugit quam, aliquam rerum quidem
+            consequuntur. Facilis, laudantium.
+        </div>
+        <h3 class="py-3">
+            $ 1.000 COL
+        </h3>
+        <button class="btn btn-outline-light btn-lg mb-5">Obtener&rarr;</button>
+
+        <!-- Acerca -->
+        <div class="my-5">
+            <h4 class="my-3">Acerca del Juego:</h4>
+            <div class="row">
+                <div class="col"> <strong>Desarrollador: </strong><br> nombre </div>
+                <div class="col"> <strong>Etiquetas: </strong><br> etiqueta1, etiqueta2 </div>
+                <div class="col"> <strong>Fecha de Lanzamiento: </strong><br> 01/ene/2020 </div>
+                <div class="col"> <strong>Reseñas: </strong><br> positivas </div>
+            </div>
+        </div>
+
+        <!-- Calificaciones -->
+        <div class="my-5">
+            <h4 class="my-3">Calificaciones:</h4>
+            <div class="row" id="ratings"></div>
+        </div>
+
+        <!-- Requisitos -->
+        <div class="my-5">
+            <h4 class="my-3">Requisitos:</h4>
+            <div class="row">
+                <div class="col bg-dark mx-3 p-5">
+                    <h5 class="mb-4">Mínimos:</h5>
+                    <div class="row justify-content-between"> <strong>Sistema Operativo: </strong> Windows 7/8/10 (64/32 Bits) </div>
+                    <div class="row justify-content-between"> <strong>Procesador: </strong> AMD E2 6110 o equivalente </div>
+                    <div class="row justify-content-between"> <strong>RAM: </strong> 4 GB Usables </div>
+                    <div class="row justify-content-between"> <strong>Gráficos: </strong> Radeon R2 </div>
+                    <div class="row justify-content-between"> <strong>Almacenamiento: </strong> 500 MB disponibles </div>
+                </div>
+                <div class="col bg-dark mx-3 p-5">
+                    <h5 class="mb-4">Recomendados:</h5>
+                    <div class="row justify-content-between"> <strong>Sistema Operativo: </strong> Windows 10 (64 Bits) </div>
+                    <div class="row justify-content-between"> <strong>Procesador: </strong> Intel Core i5 a 3,4 GHz o equivalente </div>
+                    <div class="row justify-content-between"> <strong>RAM: </strong> 8 GB Usables </div>
+                    <div class="row justify-content-between"> <strong>Gráficos: </strong> GeForce GTX 660 (2048 MB) / Radeon HD 7970 </div>
+                    <div class="row justify-content-between"> <strong>Almacenamiento: </strong> 500 MB disponibles </div>
+                </div>
+            </div>
+        </div>
+    </div>`;
+
+    // construir capturas
+    screenshots = "";
+    for (let i = 0; i < 5; i++) {
+        screenshots += `
+        <div class="carousel-item `+ (i == 0 ? "active" : "") + `">
+            <img src="https://picsum.photos/160`+ i + `/400" class="d-block w-100" alt="https://picsum.photos/1600/400">
+        </div>`;
+    }
+    document.getElementById("screenshots").innerHTML = screenshots;
+
+    ratings = "";
+    for (let i = 0; i < 3; i++) {
+        ratings += `
+        <div class="col"> 
+            <div class="card bg-dark">
+                <div class="card-body">
+                    <h5 class="card-title">Nombre</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">01 ene 2020</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h4>10/10</h4>
+                </div>
+            </div> 
+        </div>`;
+    }
+    document.getElementById("ratings").innerHTML = ratings;
 }
